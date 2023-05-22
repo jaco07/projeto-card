544d925bd3b11bc2e36d5e0b2aa1693040349ce5
jaco07 <115810219+jaco07@users.noreply.github.com>
2023-05-22 17:55:48 -0300

    Add files via upload

---
+++ projeto-card/app.js
@@ -0,0 +1,14 @@
+let numero = 0
+btn.addEventListener('click', ()=>{
+
+
+    btn.style.color = 'red'
+    btn.style.borderColor = 'red'
+    btn.style.backgroundColor = 'whiter'
+
+  
+  
+      numero += 1
+      document.querySelector('#number').textContent = numero;
+  })
+  
